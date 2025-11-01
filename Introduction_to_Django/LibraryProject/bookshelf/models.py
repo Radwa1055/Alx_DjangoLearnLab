39760552a63b082b0943293cf7378e2a2ba39bf1
Radwa1055 <radwa.r2020@gmail.com>
2025-11-01 02:12:11 +0200

    task 3

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-class book(models.Model): 
+
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    author =  models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-    
+
     def __str__(self):
         return self.title
-    
-    
