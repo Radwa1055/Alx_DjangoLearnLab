65bda8be95f6208bbb8230bc25ba8f8bfe792f68
Radwa1055 <radwa.r2020@gmail.com>
2025-11-09 01:02:40 +0200

    add

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -35,9 +35,13 @@ urlpatterns += [
 ]
 
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
+
 
