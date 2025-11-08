03a773cca34fe65df7340812904b04c40a0c39a1
Radwa1055 <radwa.r2020@gmail.com>
2025-11-09 00:50:10 +0200

    add

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -47,3 +47,24 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
+
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    library = models.ForeignKey('Library', on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = (
+            ("canaddbook", "Can add book"),
+            ("canchangebook", "Can change book"),
+            ("candeletebook", "Can delete book"),
+        )
+
+    def __str__(self):
+        return self.title
