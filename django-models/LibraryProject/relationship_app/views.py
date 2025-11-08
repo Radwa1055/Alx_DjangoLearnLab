89c0fa04bebe385049ab5661cae6caa57aeda008
Radwa1055 <radwa.r2020@gmail.com>
2025-11-08 23:25:56 +0200

    add

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 def list_books(request):
-    """Function-based view to list all books"""
+    """
+    Function-based view required to return a simple text list
+    of book titles and their authors.
+    """
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    # نص بسيط بكل سطر "Title by Author"
+    lines = [f"{book.title} by {book.author.name}" for book in books]
+    output = "\n".join(lines)
+    return HttpResponse(output, content_type="text/plain")
 
 class LibraryDetailView(DetailView):
-    """Class-based view to display library details"""
+    """
+    Class-based view using the exact import path expected by checker.
+    Displays a specific Library and its books using a template.
+    """
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = 'library'
