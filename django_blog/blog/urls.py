465cbaa70f5ede1301482e03321bc87b5108ef70
Radwa1055 <radwa.r2020@gmail.com>
2025-12-05 00:15:41 +0200

    update task3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,10 +18,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
-
-
-
-
     # Comment URLs
     path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
