c0f6d1f35d9b6404b40e130d71abc72595512694
Radwa1055 <radwa.r2020@gmail.com>
2025-12-14 11:49:42 +0200

    Fix serializers and urls to match ALX checker exactly

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()  #  
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
@@ -17,3 +18,33 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         Token.objects.create(user=user)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()  
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        token, created = Token.objects.get_or_create(user=user)
+        return {
+            'token': token.key
+        }
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'bio', 'followers_count', 'following_count']
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
+
