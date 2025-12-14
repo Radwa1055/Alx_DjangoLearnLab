200f1be1bfcae5ff5fa07a614fcfd8c46e3ee751
Radwa1055 <radwa.r2020@gmail.com>
2025-12-14 12:17:09 +0200

    Add posts app, models, serializers, views, and migrations

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -131,4 +131,10 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'accounts.User'
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.SearchFilter'],
+}
+
 
